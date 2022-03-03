e3b14ea67da5fb21f70b993ffce1456e585372d9
karo19834 <karolina1983_4@hotmail.com>
2022-03-03 14:43:50 +0100

    Grade

---
+++ src/fh/campus02/GradingTable.java
@@ -0,0 +1,27 @@
+package fh.campus02;
+
+public class GradingTable {
+    public static void main(String[] args) {
+        int points = 65;
+        String grade = gradepoint(points);
+
+        System.out.println("Your grade is: " + grade);
+
+    }
+
+    public static String gradepoint(int points) {
+        String grade;
+        if (points >= 90) {
+            grade = "Outstanding";
+        } else if (points >= 78 && points <= 89) {
+            grade = "Excellent";
+        } else if (points >= 65 && points <= 77) {
+            grade = "Acceptable";
+        } else if (points >= 51 && points <= 64) {
+            grade = "Passing";
+        } else {
+            grade = "Fail";
+        }
+        return grade;
+    }
+}
