d5fe224c0fddee4f4ce9267ce03c9617b70b89f4
karo19834 <karolina1983_4@hotmail.com>
2022-03-03 15:43:52 +0100

    ModuloWhile

---
+++ src/fh/campus02/CountDown.java
@@ -0,0 +1,17 @@
+package fh.campus02;
+
+public class CountDown {
+    public static void main(String[] args) {
+     count (12);
+
+    }
+    public static void count (int firstnumber) {
+
+        while (firstnumber >= 0) {
+            System.out.println( firstnumber);
+            firstnumber--;
+        }
+
+
+    }
+}
---
+++ src/fh/campus02/Teilbar.java
@@ -0,0 +1,14 @@
+package fh.campus02;
+
+public class Teilbar {
+    public static void main(String[] args) {
+        int firstnumber = 76;
+        int secondnumber = 3;
+
+        int result = firstnumber % secondnumber ;
+
+        if (result ==0 ) {
+            System.out.println( "Zahl ist dividierbar durch 3. ");
+        }else System.out.println( " Zahl ist nicht dividierbar durch 3. ");
+    }
+}
